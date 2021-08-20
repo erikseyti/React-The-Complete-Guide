6c24789c5112aad361fc4ffa708bdb21efee7f6d
Erik Seyti <erikseyti@gmail.com>
2021-08-20 15:20:57 -0300

    Example of how to use only 1 useState to manage data instead of many values for each change value

--- 01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ 01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -2,23 +2,39 @@ import "./ExpenseForm.css";
 import { useState } from "react";
 
 const ExpenseForm = () => {
-
-   const [enteredTitle, setEnteredTitle] = useState('');
-   const [enteredAmount, setEnteredAmount] = useState('');
-   const [enteredDate, setEnteredDate] = useState('');
+  //    const [enteredTitle, setEnteredTitle] = useState('');
+  //    const [enteredAmount, setEnteredAmount] = useState('');
+  //    const [enteredDate, setEnteredDate] = useState('');
+  const [userInput, setUserInput] = useState({
+    enteredTitle: "",
+    enteredAmount: "",
+    enteredDate: "",
+  });
 
   const titleChangeHandler = (event) => {
     console.log("title Changed!");
-    setEnteredTitle(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredTitle: event.target.value,
+    });
+    // setEnteredTitle(event.target.value);
   };
 
   const amountChangeHandler = (event) => {
-    setEnteredAmount(event.target.value)
+    // setEnteredAmount(event.target.value)
+    setUserInput({
+      ...userInput,
+      setEnteredAmount: event.target.value,
+    });
   };
 
   const dateChangeHandler = (event) => {
-    setEnteredDate(event.target.value)
-  }
+    // setEnteredDate(event.target.value)
+    setUserInput({
+      ...userInput,
+      setEnteredDate: event.target.value,
+    });
+  };
 
   return (
     <form className="new-expense__form">
@@ -30,12 +46,22 @@ const ExpenseForm = () => {
 
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" onChange={amountChangeHandler} steap="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            onChange={amountChangeHandler}
+            steap="0.01"
+          />
         </div>
 
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" onChange={dateChangeHandler} max="2022-12-31" />
+          <input
+            type="date"
+            min="2019-01-01"
+            onChange={dateChangeHandler}
+            max="2022-12-31"
+          />
         </div>
       </div>
 
