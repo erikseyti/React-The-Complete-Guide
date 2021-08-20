a345aa1963f81286ce2b687aec31e8b899b2d014
Erik Seyti <erikseyti@gmail.com>
2021-08-20 17:28:50 -0300

    Changing the logic of ExpenseItems to use an map operator to loop a list of expenses to be displayed

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -33,7 +33,7 @@ const App = () => {
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses expenses={expenses}></Expenses>
+      <Expenses items={expenses}></Expenses>
 
     </div>
   );
