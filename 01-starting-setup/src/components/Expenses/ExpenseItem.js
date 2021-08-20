24dedfdb1699df5596aeccbc9aea970955d9d1cd
Erik Seyti <erikseyti@gmail.com>
2021-08-20 14:12:17 -0300

    Add Example to when click a button the props.title of the component is updated with new value

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,22 @@
 import Card from "../UI/Card";
+import { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  // useState must only be called on the upperside of the main react function.
+  const [title, setTitle] = useState(props.title);
 
   const clickHandler = () => {
-    console.log('Clicked!')
-  }
+    setTitle("updated!");
+    console.log(title);
+  };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
