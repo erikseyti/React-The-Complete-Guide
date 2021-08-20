fb3d20e906df6cd26fe62520a75a1c5f9f85081b
Erik Seyti <erikseyti@gmail.com>
2021-08-20 15:40:20 -0300

    Example of how to handle with a submit of a form

--- 01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ 01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -45,8 +45,20 @@ const ExpenseForm = () => {
     // });
   };
 
+  const submitHandler = (event) => {
+      // prevent the submit of the form be trigger
+      event.preventDefault();
+      const expenseData = {
+        title: enteredTitle,
+        amount: enteredAmount,
+        date: new Date(enteredDate)
+      }
+
+      console.log(expenseData);
+  }
+
   return (
-    <form className="new-expense__form">
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
